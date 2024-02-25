2c11aacae0ef3d6eb2fe44a3f4b0d9c6dd9c11a2
yoo <edel1212@naver.com>
2024-02-25 18:13:34 +0900

    파일 첨부 메일

--- src/main/java/com/yoo/springBootMailStudy/service/EmailService.java
+++ src/main/java/com/yoo/springBootMailStudy/service/EmailService.java
@@ -1,10 +1,12 @@
 package com.yoo.springBootMailStudy.service;
 
-import com.yoo.springBootMailStudy.mail.MailDTO;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,4 +23,28 @@ public class EmailService {
         javaMailSender.send(msg);
     }
 
+    public void mailSendToHTMLAddFile(){
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // multipart 모드 활성화
+
+            helper.setTo("edel1212@naver.com");
+            helper.setSubject("흑곰님입니다.");
+
+            // HTML 콘텐츠를 문자열로 작성
+            String htmlContent = "<h1>안녕하세요, 흑곰님입니다!</h1>" +
+                    "<p>!!!! 내용입니다.</p>" +
+                    "<p>HTML 형식의 <strong>이메일</strong>을 보내드립니다.</p>"; // 이미지를 참조하는 HTML 태그
+
+            helper.setText(htmlContent, true); // true를 설정하여 HTML 메일로 인식하게 함
+
+            // 이미지 파일을 첨부
+            ClassPathResource image = new ClassPathResource("static/img/test.jpg");
+            helper.addInline("test.png", image);
+
+            javaMailSender.send(mimeMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/yoo/springBootMailStudy/service/EmailServiceTest.java
+++ src/test/java/com/yoo/springBootMailStudy/service/EmailServiceTest.java
@@ -1,12 +1,10 @@
 package com.yoo.springBootMailStudy.service;
 
-import com.yoo.springBootMailStudy.mail.MailDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EmailServiceTest {
@@ -16,7 +14,13 @@ class EmailServiceTest {
 
     @Test
     @DisplayName("이메일 전송")
-    void name() {
+    void simpleMailSender() {
         emailService.mailSend();
+    }
+
+    @Test
+    @DisplayName("HMLT 본문 및 이미지 첨부")
+    void mailToHTML() {
+        emailService.mailSendToHTMLAddFile();
     }
 }
