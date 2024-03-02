e0292aae7173f7d4963d4bebf4f7e40be76c77f2
yoo <edel1212@naver.com>
2024-03-02 12:18:58 +0900

    cid 삽질 방지 주석 추가

--- src/main/java/com/yoo/springBootMailStudy/service/EmailService.java
+++ src/main/java/com/yoo/springBootMailStudy/service/EmailService.java
@@ -52,7 +52,7 @@ public class EmailService {
             helper.setSubject("흑곰님입니다.");
 
             // HTML 콘텐츠를 문자열로 작성
-            String htmlContent = "<img src='cid:logo'>"; // 이미지를 참조하는 HTML 태그
+            String htmlContent = "<h1>메일입니다!!</h1>"; // 이미지를 참조하는 HTML 태그
 
             helper.setText(htmlContent, true); // true를 설정하여 HTML 메일로 인식하게 함
 
@@ -66,5 +66,36 @@ public class EmailService {
         }
     }
 
+    public void mailSendToHTMLAddCid(){
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // multipart 모드 활성화
+
+            helper.setTo("edel1212@naver.com");
+            helper.setSubject("흑곰님입니다.");
+
+            // HTML 콘텐츠를 문자열로 작성
+            String htmlContent = "<img src='cid:logo'>"; // 이미지를 참조하는 HTML 태그
+
+            helper.setText(htmlContent, true); // true를 설정하여 HTML 메일로 인식하게 함
+
+
+            /**************************/
+            /***  🔥 초 삽질했었음 ...  */
+            // setText()를 통해 먼저 선행으로 Dom요소를 만든 후 cid를 추가
+            // 파일 추가 순서로 가야지 진행됬었음
+            // 이미지 파일 첨부를 그 윗 단계  String htmlContent = "<img src='cid:logo'>"; 이전 에서
+            // 진행 하여 이미지 첨부가 안되고 첨부파일 내 attachg(0).txt 만들어가는 이슈가 있었다 ..
+            /**************************/
+
+            // 이미지 파일을 첨부  !!!! ⭐ 순서 초 중요!!!
+            ClassPathResource image = new ClassPathResource("static/img/test.jpg");
+            helper.addInline("logo", image);
+
+            javaMailSender.send(mimeMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
